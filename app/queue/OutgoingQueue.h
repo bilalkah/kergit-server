bbc213d17b7348dc261eca9ee75abec6c2e640e9
Bilal Kahraman <kahramannbilal@gmail.com>
2025-11-25 00:27:20 +0300

    Adjust outconsumer and set it in socket server

--- app/queue/OutgoingQueue.h
+++ app/queue/OutgoingQueue.h
@@ -4,17 +4,28 @@
 #include "app/queue/ThreadSafeCmdQueue.h"
 #include "domains/ids/Ids.h"
 
+#include <functional>
+#include <string>
+#include <variant>
+
 namespace net {
 struct PerSocketData;
 }
 
-struct OutgoingMessage {
+struct DirectMessage {
     ConnId conn_id;
-
     std::string payload;
 
-    std::function<void(net::PerSocketData&)> apply_psd;
+    std::function<void(net::PerSocketData*)> apply_psd;
 };
+
+struct PublishMessage {
+    std::string topic;
+    std::string payload;
+};
+
+// Outgoing messages are one of these two
+using OutgoingMessage = std::variant<DirectMessage, PublishMessage>;
 
 using OutgoingQueue = ThreadSafeQueue<OutgoingMessage>;
 
