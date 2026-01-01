42167f1c0438c472f1a5114c3dbac0523d7d85cc
Bilal Kahraman <kahramannbilal@gmail.com>
2026-01-01 12:33:32 +0000

    Optimize fanout

--- net/NetworkRouter.cpp
+++ net/NetworkRouter.cpp
@@ -5,29 +5,46 @@ namespace net {
 void NetworkRouter::register_stack(std::unique_ptr<NetworkStack> stack) {
     auto id = stack->id();
     net_stacks_by_id_.emplace(id, std::move(stack));
+    outbound_sinks_by_id_.emplace(id, net_stacks_by_id_[id]->outbound_sink());
 }
 
 void NetworkRouter::push(const outbound::OutgoingMessage& msg) {
-    for (const auto& cid : msg.target.conns) {
-        auto it = net_stacks_by_id_.find(cid.netstack_id);
-        if (it == net_stacks_by_id_.end()) continue;
+    // group connections by netstack_id
+    std::unordered_map<NetStackId, std::vector<GlobalConnId>> grouped;
 
-        outbound::OutgoingMessage forwarded{.target = outbound::Target::one(cid),
+    for (const auto& cid : msg.target.conns) {
+        grouped[cid.netstack_id].push_back(cid);
+    }
+
+    // send once per netstack
+    for (auto& [netstack_id, conns] : grouped) {
+        auto it = outbound_sinks_by_id_.find(netstack_id);
+        if (it == outbound_sinks_by_id_.end()) continue;
+
+        outbound::OutgoingMessage forwarded{.target = outbound::Target{std::move(conns)},
                                             .action = msg.action};
 
-        it->second->outbound_sink().push(std::move(forwarded));
+        it->second.push(std::move(forwarded));
     }
 }
 
 void NetworkRouter::push(outbound::OutgoingMessage&& msg) {
+    // group connections by netstack_id
+    std::unordered_map<NetStackId, std::vector<GlobalConnId>> grouped;
+
     for (const auto& cid : msg.target.conns) {
-        auto it = net_stacks_by_id_.find(cid.netstack_id);
-        if (it == net_stacks_by_id_.end()) continue;
+        grouped[cid.netstack_id].push_back(cid);
+    }
 
-        outbound::OutgoingMessage forwarded{.target = outbound::Target::one(cid),
-                                            .action = std::move(msg.action)};
+    // send once per netstack
+    for (auto& [netstack_id, conns] : grouped) {
+        auto it = outbound_sinks_by_id_.find(netstack_id);
+        if (it == outbound_sinks_by_id_.end()) continue;
 
-        it->second->outbound_sink().push(std::move(forwarded));
+        outbound::OutgoingMessage forwarded{.target = outbound::Target{std::move(conns)},
+                                            .action = msg.action};
+
+        it->second.push(std::move(forwarded));
     }
 }
 
