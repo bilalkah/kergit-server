102beee4d808d0f151da5b6e41b7f46f0a69ad93
Bilal Kahraman <kahramannbilal@gmail.com>
2026-01-02 18:03:25 +0000

    Adjust create_channel command

--- app/commands/channel/CreateChannelCommand.h
+++ app/commands/channel/CreateChannelCommand.h
@@ -1,24 +1,15 @@
-#ifndef APP_COMMANDS_CREATECHANNELCOMMAND_H
-#define APP_COMMANDS_CREATECHANNELCOMMAND_H
+#ifndef APP_COMMANDS_CHANNEL_CREATECHANNELCOMMAND_H
+#define APP_COMMANDS_CHANNEL_CREATECHANNELCOMMAND_H
 
 #include "app/commands/ICommand.h"
-#include "domains/Hub.h"
-
-#include <string>
 
 namespace app {
 
 class CreateChannelCommand : public ICommand {
    public:
-    CreateChannelCommand(ServiceObjects& svc_objs);
-    void execute(CommandContext&) override;
-
-   private:
-    bool has_privilege(const net::Snapshot& snapshot, const HubId& hub_id);
-
-    ServiceObjects& services_;
+    CommandResult execute(CommandContext& ctx, const CommandInput cmd) override;
 };
 
 }  // namespace app
 
-#endif  // APP_COMMANDS_CREATECHANNELCOMMAND_H
+#endif  // APP_COMMANDS_CHANNEL_CREATECHANNELCOMMAND_H
